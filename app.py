24b3a934760df7c47e13067e69433b946d9b1484
naisarg14 <naisarg.patel14@hotmail.com>
2022-12-02 09:34:43 +0530

    Update 2/12/2022

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
+from helpers import apology
 
 # Configure application
 app = Flask(__name__)
@@ -21,7 +22,7 @@ Session(app)
 db = SQL("sqlite:///students.db")
 
 
-CLASSES = ["Class-9", "Class-10", "Class-11", "Class-12"]
+GRADES = ["Class-9", "Class-10", "Class-11", "Class-12"]
 SUBJECTS = ["Mathematics", "Science"]
 
 
@@ -40,10 +41,10 @@ def admissions():
         #TODO
 
 
-@app.route("/student")
+@app.route("/student", methods=["GET", "POST"])
 def student():
     if request.method == "GET":
-        return render_template("forms.html", classes=CLASSES, subjects=SUBJECTS)
+        return render_template("forms.html", grades=GRADES, subjects=SUBJECTS)
     if request.method == "POST":
         first_name = request.form.get("first_name")
         middle_name = request.form.get("middle_name")
@@ -55,11 +56,20 @@ def student():
         address2 = request.form.get("address2")
         grade = request.form.get("grade")
 
+        if not first_name or not last_name or not birthdate or not email or not own_ph or not address or not grade:
+            return apology("Required Fields Cannot Be Empty")
+
+        if grade not in GRADES:
+            return apology("Error")
+
+        db.execute("INSERT INTO student (first_name, middle_name, last_name, birthdate, email, own_ph, address, address2, grade) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", first_name, middle_name, last_name, birthdate, email, int(own_ph), address, address2, grade)
+
+        return apology("Succeess")
 
 @app.route("/subject")
 def subject():
     if request.method == "GET":
-        return render_template("forms.html", classes=CLASSES, subjects=SUBJECTS)
+        return render_template("forms.html", subjects=SUBJECTS)
     if request.method == "POST":
         ...
         #TODO
